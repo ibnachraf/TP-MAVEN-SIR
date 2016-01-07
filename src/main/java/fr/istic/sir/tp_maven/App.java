bea68f1e7f01a017933ca794062eb00abd701c9e
ibnachraf <ibnmrabet.achraf@gmail.com>
2016-01-07 08:27:43 +0100

    first commit

---
+++ src/main/java/fr/istic/sir/tp_maven/App.java
@@ -0,0 +1,45 @@
+package fr.istic.sir.tp_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	float p;
+    public static void main( String[] args )
+    {
+    	/**
+    	 * affiche hello word
+    	 */
+        System.out.println( "Hello World!" );
+    }
+    
+    void methode(int i){
+    	String s =";";
+    	int u=3;
+    	System.out.println("hellooo word 2");
+    	
+    	if(u==3){
+    		System.out.println("1");
+    	}
+    	
+    	else if (u==4){
+    		System.out.println("5");
+    	}
+    	
+    	else if (u==4){
+    		System.out.println("5");
+    	}
+    	else if (u==4){
+    		System.out.println("5");
+    	}
+    	else if (u==4){
+    		System.out.println("5");
+    	}
+    	else
+    		System.out.println("00000");
+    	
+    	
+    }
+}
